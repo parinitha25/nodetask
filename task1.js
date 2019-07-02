8428c1284ccf7e91731a22133ed31fc134f2797b
parinitha25 <pnarayanaostb1@gmail.com>
2019-07-02 13:06:42 +0530

    day task

---
+++ task1.js
@@ -0,0 +1,28 @@
+const day = new Date().getDay()+1;
+
+switch (day)
+  {
+    case 0:
+        console.log("Today is Sunday");
+        break;
+    case 1:
+        console.log("Today is Monday");
+        break;
+    case 2:
+        console.log("Today is Tuesday");
+        break;
+    case 3:
+        console.log("Today is Wedensday");
+        break;
+    case 4:
+        console.log("Today is Thursday");
+        break;
+    case 5:
+        console.log("Today is Friday");
+        break;
+    case 6:
+        console.log("Today is Saturday");
+        break;
+    default:
+        break;
+  }
---
+++ task1a.js
@@ -0,0 +1,21 @@
+
+  var i="hello"
+  switch(i){
+    case "hi":
+        console.log("string are equal to hi");
+        break;
+    case "hello":
+        console.log("string are equal to  hello");
+        break;
+    default:
+        console.log("the value of i is not match with any values");
+        break;
+  }
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+  console.log(Math.random());
+
+Math.floor(Math.random() * 6) + 1  
+console.log(Math.random());
+   
